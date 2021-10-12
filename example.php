f972ef6c26a0d44475b1cc27a0c9579684058000
Valentin Gerbey <38650680+WalentinG@users.noreply.github.com>
2021-10-12 15:00:37 +0300

    Update example.php

--- example.php
+++ example.php
@@ -11,11 +11,11 @@ use TripSorter\TripSorter;
 
 include __DIR__ . '/vendor/autoload.php';
 
-$trips = TripSorter::fromCards(
+$tripSorter = TripSorter::fromCards(
     new AirplaneTicket(new Airport('Stockholm'), new Airport('New York JFK'), '22', '7B'),
     new BusTicket(new RailwayStation('Barcelona'), new Airport('Gerona')),
     new TrainTicket(new RailwayStation('Madrid'), new RailwayStation('Barcelona'), '78A', '45B'),
     new AirplaneTicket(new Airport('Gerona'), new Airport('Stockholm'), '45B', '3A'),
 );
 
-echo $trips->description(' ')->value;
+echo $tripSorter->description(' ')->value;
